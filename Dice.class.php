7f5a7b4aee8564c88ef0f47b1ba531d19a42b9b5
Peter 4A5A <498883@edu.rocmn.nl>
2017-11-23 09:20:57 +0100

    Wrote the head class

---
+++ Dice.class.php
@@ -0,0 +1,35 @@
+<?php
+
+  class Dice {
+    private $startNumber;
+    private $endNumber;
+
+    private $randomNumber;
+
+    public function __construct($startNumber, $endNumber) {
+      $this->startNumber = $startNumber;
+      $this->endNumber = $endNumber;
+    }
+
+    private function getStartNumber() {
+      return($this->startNumber);
+    }
+
+    private function getEndNumber() {
+      return($this->endNumber);
+    }
+
+    protected function generateNumber() {
+      $this->randomNumber = rand($this->getStartNumber(), $this->getEndNumber());
+      return($this->randomNumber);
+    }
+
+    public function getNumber() {
+      return($this->generateNumber());
+    }
+
+    // abstract public function roll();
+  }
+
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  require_once 'Dice.class.php';
+  $Dice = new Dice(1,6);
+  echo $Dice->getNumber();
+?>
